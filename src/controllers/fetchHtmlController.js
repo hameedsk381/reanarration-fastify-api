caa488740f815cfe2c169f6126eb2c5a956508f6
hameed Shaik <hameedsk381@gmail.com>
2024-03-30 11:19:57 +0530

    revereted to old api

--- src/controllers/fetchHtmlController.js
+++ src/controllers/fetchHtmlController.js
@@ -1,38 +1,51 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
-import { JSDOM } from 'jsdom';
 import juice from 'juice';
 import { v4 as uuidv4 } from 'uuid';
 import { Savedoc } from '../models/Htmldocs.js';
-import { Readability } from '@mozilla/readability';
+
 export const downloadContent = async (request, reply) => {
     const { url } = request.body;
 
     try {
         const existingHtmlDoc = await Savedoc.findOne({ url });
         if (existingHtmlDoc) {
-          console.log('using existing page');
-          reply.header('Content-Type', 'text/html').send(existingHtmlDoc.htmldoc);
+            console.log('using existing page')
+            reply.header('Content-Type', 'text/html').send(existingHtmlDoc.htmldoc);
         } else {
-          // Fetch the HTML content of the URL
-    const response = await axios.get(url);
+            const response = await axios.get(url);
+            const $ = cheerio.load(response.data);
 
-    // Parse HTML content using JSDOM
-    const { document } = new JSDOM(response.data).window;
+            $('link[rel="icon"], link[rel="shortcut icon"]').remove();
+            $('svg').attr({ 'width': '50', 'height': '50' });
+            $('*').each((index, element) => {
+                const el = $(element);
+                el.removeAttr('onmouseover').removeAttr('onmouseout');
+                if (el.css('position') === 'fixed' || el.css('position') === 'sticky') {
+                    el.css('position', 'static');
+                }
+                const existingDataId = el.attr('data-id');
+                el.attr('data-id', existingDataId || uuidv4());
+            });
 
-    // Apply readability.js to extract main content
-    const reader = new Readability(document);
-    const article = reader.parse();
+           
 
+            $('header, footer, sidebar').remove(); // Remove header, footer, and sidebar tags
 
-    // Send the modified HTML content as the response
-    reply.header('Content-Type', 'text/html').send(article.content);
+            const htmlContent = juice($.html());
+           
+            const newHtmlDoc = new Savedoc({
+                htmldoc: htmlContent,
+                url: url
+            });
+            await newHtmlDoc.save();
+            console.log('using fetched page')
+            reply.header('Content-Type', 'text/html').send(htmlContent);
         }
-  
+        
         
     } catch (error) {
         console.error(`This page cannot be renarrated at the moment: ${error.message}`);
-        reply.status(500).send(error);
+        reply.status(500).send('This page cannot be renarrated at the moment');
     }
 };
-
